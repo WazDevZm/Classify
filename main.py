8643badad0ff54cb024a78f5f7920c7515f890c2
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-05-19 20:06:51 +0200

    initial commit of the code

---
+++ main.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+# this is an mage clssifer built using opencv and streamlit
+# Load the pre-trained model
+from tensorflow.keras.applications.mobilenet_v2 import (
+    MobileNetV2, # already trained model, light model
+    # MobileNetV2 is a lightweight model, suitable for mobile and edge devices
+    preprocess_input,
+    decode_predictions,
+)
+#tensorflow is being used to load pre-trained model, we are not traing from scratch
+from PIL import Image
+
+def load_model():
+    model = MobileNetV2(weights='imagenet')
+    return model
+# omobilente vs is a convolutional neural network architecture that is designed to be lightweight and efficient, making it suitable for mobile and edge devices. It is a variant of the original MobileNet architecture, which was introduced by Google in 2017. MobileNetV2 builds upon the success of its predecessor by introducing several improvements and optimizations.
+def preprocess_image(image):
+    # corecy frmat of the image to be passed to the model
+    img = np.array(image) # cobnverts h imge into an arra of numbers
+    
